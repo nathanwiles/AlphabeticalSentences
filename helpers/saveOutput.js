40f4840818e6bfad1afa2ae6364d325b6383d823
nathanwiles <nathanawiles@gmail.com>
2024-02-11 14:04:03 -0500

    turn into promise to allow for syncronous execution.

--- helpers/saveOutput.js
+++ helpers/saveOutput.js
@@ -1,44 +1,54 @@
 const fs = require('fs');
 
 const saveOutput = (alphabetizedSections, fileName) => {
-  let output = '';
+  return new Promise((resolve, reject) => {
+    let output = '';
 
-  if (alphabetizedSections.length > 1) {
-    // mark the end of each section with a line of dashes
-    for (let section of alphabetizedSections) {
-      output += section;
-      output += "\n------------------------------------------------\n";
+    if (alphabetizedSections.length > 1) {
+      // mark the end of each section with a line of dashes
+      for (let section of alphabetizedSections) {
+        output += section;
+        output += "\n------------------------------------------------\n";
+      }
+    } else {
+      // if there is only one section, don't add the line of dashes
+      output = alphabetizedSections[0];
     }
-  } else {
-    // if there is only one section, don't add the line of dashes
-    output = alphabetizedSections[0];
-  }
 
-  let i = 1;
-  if (!fileName) {
-    fileName = 'output';
-    filePath = './outputs/output.txt';
-  } else {
-    // remove any file extension from the filename
-    fileName = fileName.replace(/\.[^/.]+$/, "");
-    filePath = `./outputs/${fileName}.txt`;
-  }
+    // set the filename and filepath
+    let i = 1;
+    if (!fileName) {
+      // use a default filename
+      fileName = 'output';
+      filePath = './outputs/output.txt';
+    } else {
+      // remove any file extension from the filename
+      fileName = fileName.replace(/\.[^/.]+$/, "");
+      // set the file path
+      filePath = `./outputs/${fileName}.txt`;
+    }
 
-  // create a directory to store the output files if it doesn't exist
-  if (!fs.existsSync('./outputs')) {
-    fs.mkdirSync('./outputs');
-  }
+    // create a the outputs directory if it doesn't exist
+    if (!fs.existsSync('./outputs')) {
+      fs.mkdirSync('./outputs');
+    }
 
-  // find the next available filename
-  while (fs.existsSync(filePath)) {
-    filePath = `./outputs/${fileName}_${i}.txt`;
-    i++;
-  }
+    // check if the file already exists
+    while (fs.existsSync(filePath)) {
+      // if the file already exists, add an incrementing number to the filename
+      filePath = `./outputs/${fileName}_${i}.txt`;
+      i++;
+    }
 
-  // write the output to a file
-  fs.writeFile(filePath, output, (err) => {
-    if (err) throw err;
-    console.log(`The result has been saved to '${filePath}'.\n`);
+    // write the output to the file
+    fs.writeFile(filePath, output, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        console.log(`The result has been saved to '${filePath}'.\n`);
+        resolve();
+      }
+    });
   });
 }
 
