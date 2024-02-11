7a06d4e00f386575c38f9718ebe0267e0438ecbe
nathanwiles <nathanawiles@gmail.com>
2024-02-11 14:04:26 -0500

    change variable name for clarity

--- helpers/seperateSections.js
+++ helpers/seperateSections.js
@@ -1,12 +1,12 @@
 /**
  * Function to seperate the sentences into sections
- * @param {string} sentences
+ * @param {string} text
  * @returns {array of strings} seperatedSections
  */
 
-function seperateSections(sentences) {
+function seperateSections(text) {
   // Split the sentences into an array, split by newline
-  const sentencesArray = sentences.split('------------------------------------------------');
+  const sentencesArray = text.split('------------------------------------------------');
   // Remove any empty strings from the array
   const filteredSentencesArray = sentencesArray.filter(sentence => sentence.trim() !== '');
   // Return the filteredSentencesArray
